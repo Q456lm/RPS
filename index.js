3fa55290c9cac29e6f04a1fbbffb1fe6605e2d23
Q456lm <cbbunting1@my.waketech.edu>
2025-05-06 14:00:57 -0400

    Started code with making buttons and images and defining them in JS

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+const but1 = document.getElementById("Rock1");
+const but2 = document.getElementById("Paper1");
+const but3 = document.getElementById("Scissor1");
+const img1 = document.getElementById("image1");
